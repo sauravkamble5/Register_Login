b04276ecb73cd614de5d902b8fb5551dcb48ff2b
Saurav Kamble <srvkmbl@gmail.com>
2023-06-29 12:05:56 +0530

    add

---
+++ server/index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const EmployeeModel = require('./models/Employee')
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+mongoose.connect('mongodb://localhost:27017/employee')
+.then( () => console.log("MongoDB is connected") )
+.catch(err => console.log(err))
+    
+
+
+app.listen(3001, () => {
+    console.log("Server is running");
+})
+
+app.post('/register', (req, res) => {
+EmployeeModel.create(req.body)
+.then(employees => res.json(employees))
+.catch(err => res.json(err))
+})
+
+app.post('/login', (req, res) => {
+    const {email, password} = req.body;
+    EmployeeModel.findOne({email:email})
+    .then(user => {
+        if(user){
+            if(user.password === password){
+                res.json("success")
+            }else{
+                res.json("Incorrect Password")
+            }
+        } else{
+            res.json("User not found")
+        }
+    })
+})
